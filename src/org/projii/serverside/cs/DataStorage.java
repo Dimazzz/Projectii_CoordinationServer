9f17e92f2f0d3b89151a9b2b98546fc1612db07b
Yau Ref <yau.ref@gmail.com>
2012-11-22 18:11:06 +0400

    Created data storage

---
+++ src/org/projii/serverside/cs/DataStorage.java
@@ -0,0 +1,22 @@
+package org.projii.serverside.cs;
+
+import org.projii.commons.space.Map;
+import org.projii.commons.spaceship.Spaceship;
+
+import java.util.List;
+
+public class DataStorage {
+
+    public List<Spaceship> getUserShips(long userid) {
+        return null;
+    }
+
+    public List<Map> getGames() {
+        return null;
+    }
+
+    public Map getGame(long id) {
+        return null;
+    }
+
+}
