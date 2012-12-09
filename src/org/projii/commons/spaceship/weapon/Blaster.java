e6f0d8ae09fca38ef93ba402367e63b961c7438f
Yau Ref <yau.ref@gmail.com>
2012-12-10 00:20:24 +0400

    Commons moved to external library

--- src/org/projii/commons/spaceship/weapon/Blaster.java
+++
@@ -1,8 +0,0 @@
-package org.projii.commons.spaceship.weapon;
-
-public class Blaster extends Weapon {
-
-    public Blaster(int id, Bullet bullet, int rate) {
-        super(id, bullet, rate);
-    }
-}
--- src/org/projii/commons/spaceship/weapon/Laser.java
+++
@@ -1,9 +0,0 @@
-package org.projii.commons.spaceship.weapon;
-
-public class Laser extends Weapon {
-
-    public Laser(int id, Bullet bullet, int rate) {
-        super(id, bullet, rate);
-    }
-
-}
